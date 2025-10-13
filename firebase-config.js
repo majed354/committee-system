b3ba1ee68faf2f7281b96eaa79c7a5105dbe3471
majed354 <majed1427h@gmail.com>
2025-10-13 12:48:52 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,10 @@
 const firebaseConfig = {
   apiKey: "AIzaSyBdNamS49AFq_U9nO95p5KvQhhmdng0c7I",
   authDomain: "my-netlify-app-9ce62.firebaseapp.com",
-  databaseURL: "https://my-netlify-app-9ce62-default-rtdb.firebaseio.com/",
+  databaseURL: "https://my-netlify-app-9ce62-default-rtdb.firebaseio.com",
   projectId: "my-netlify-app-9ce62",
   storageBucket: "my-netlify-app-9ce62.firebasestorage.app",
-  messagingSenderId: 532298452257",
+  messagingSenderId: "532298452257",
   appId: "1:532298452257:web:5a47b683ba357c4fbc8260"
 };
 
